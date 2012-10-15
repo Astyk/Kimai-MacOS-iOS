1a93a8d8062eeb20b751580c14f66e74393a01df
Vinzenz Weber <vinzenz@blockhaus-medien.at>
2012-10-15 14:35:14 +0200

    Initial Commit

---
+++ kimai/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  kimai
+//
+//  Created by Vinzenz-Emanuel Weber on 15.10.12.
+//  Copyright (c) 2012 blockhaus medienagentur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ kimai/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  kimai
+//
+//  Created by Vinzenz-Emanuel Weber on 15.10.12.
+//  Copyright (c) 2012 blockhaus medienagentur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}

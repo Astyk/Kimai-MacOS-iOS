1900946fc8fa18ca12dd151cb52cda21b1bccaa9
Vinzenz Weber <vinzenz@blockhaus-medien.at>
2012-10-19 11:17:21 +0200

    play around with running applications

---
+++ kimai/ApplicationObserver/RunningApplicationsController.h
@@ -0,0 +1,13 @@
+//
+//  RunningApplicationsController.h
+//  kimai
+//
+//  Created by Vinzenz-Emanuel Weber on 19.10.12.
+//  Copyright (c) 2012 blockhaus medienagentur. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RunningApplicationsController : NSArrayController
+
+@end
